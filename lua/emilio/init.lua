ee2aa60fe1db1d542a6d2678564464be61159014
emiliopalmerini <emilio.palmerini@gmail.com>
2024-02-18 15:52:34 +0100

    remap and set

---
+++ lua/emilio/init.lua
@@ -0,0 +1,21 @@
+-- remap.lua
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
+
+-- set.lua
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true 
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
