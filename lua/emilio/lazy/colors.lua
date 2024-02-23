347b16f96b8e6699d397cfe013fd664460f4218b
emiliopalmerini <emilio.palmerini@gmail.com>
2024-02-23 21:32:56 +0100

    complete setup plugin

--- lua/emilio/lazy/colors.lua
+++ lua/emilio/lazy/colors.lua
@@ -1,9 +1,9 @@
 return{
     {
-      "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-      opts = {},
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
     },
     {
         'rose-pine/neovim',
@@ -12,4 +12,16 @@ return{
             vim.cmd('colorscheme rose-pine')
         end
     },
+    config = function()
+        function ColorMyPencils(color) 
+            color = color or "rose-pine"
+            vim.cmd.colorscheme(color)
+
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+        end
+
+        ColorMyPencils()
+    end
 }
--- lua/emilio/lazy/refactoring.lua
+++ lua/emilio/lazy/refactoring.lua
@@ -1,3 +1,9 @@
 return{
     "theprimeagen/refactoring.nvim",
+    config = function()
+        require('refactoring').setup({})
+
+        vim.api.nvim_set_keymap("v", "<leader>ri", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], 
+        {noremap = true, silent = true, expr = false})
+    end
 }
