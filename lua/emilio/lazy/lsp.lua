365e6d33de526060d62f5deca979b2662a70db78
emiliopalmerini <emilio.palmerini@gmail.com>
2024-02-25 17:33:58 +0100

    setup lsp

--- lua/emilio/lazy/lsp.lua
+++ lua/emilio/lazy/lsp.lua
@@ -1,15 +1,3 @@
-local function attach (opts)
-
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end
-
 return{
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -19,11 +7,12 @@ return{
 
     config = function()
         require("mason").setup()
-        require("mason-lspconfig").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "golangci_lint_ls",
+            },
+        })
 
-        -- After setting up mason-lspconfig you may set up servers via lspconfig
-        -- require("lspconfig").lua_ls.setup {}
-        -- require("lspconfig").rust_analyzer.setup {}
-        -- ...end
     end
 }
