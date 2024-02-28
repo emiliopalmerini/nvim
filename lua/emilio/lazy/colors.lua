429049b04e9c8baaec437f4e097630bf3275e596
emiliopalmerini <emilio.palmerini@gmail.com>
2024-02-28 19:09:21 +0100

    make nvim transparent

--- lua/emilio/lazy/colors.lua
+++ lua/emilio/lazy/colors.lua
@@ -1,9 +1,30 @@
 return {
     "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    lazy = false,
+    priority = 1000,
     config = function()
-        -- load the colorscheme here
+        require("tokyonight").setup({
+            style = "night",
+            light_style = "day",
+            transparent = true,
+            terminal_colors = true,
+            styles = {
+                comments = { italic = true },
+                keywords = { italic = true },
+                functions = {},
+                variables = {},
+                sidebars = "transparent",
+                floats = "transparent",
+            },
+            sidebars = { "qf", "help" },
+            day_brightness = 0.3,
+            hide_inactive_statusline = false,
+            dim_inactive = false,
+            lualine_bold = false,
+
+            on_colors = function(colors) end,
+            on_highlights = function(highlights, colors) end,})
+
         vim.cmd([[colorscheme tokyonight]])
     end,
 }
