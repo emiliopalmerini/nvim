c5808148fc2507601c1c164965e0ffb5ed2ad3c5
emiliopalmerini <emilio.palmerini@gmail.com>
2024-03-10 16:48:34 +0100

    plan update

--- lua/emilio/lazy/fugitive.lua
+++ lua/emilio/lazy/fugitive.lua
@@ -21,8 +21,8 @@ return {
                 end, opts)
 
                 -- rebase always
-                vim.keymap.set("n", "<leader>gP", function()
-                    vim.cmd.Git({'pull', '--rebase'})
+                vim.keymap.set("n", "<leader>P", function()
+                    vim.cmd.Git({'pull',  '--rebase'})
                 end, opts)
 
                 -- NOTE: It allows me to easily set the branch i am pushing and any tracking
