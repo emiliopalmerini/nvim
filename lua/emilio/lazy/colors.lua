717e19a376dd5390772e164fb1e38ffd0a05ab5a
emiliopalmerini <emilio.palmerini@gmail.com>
2024-04-01 19:30:47 +0200

    feat(colors): simplify folke/tokyonigh

--- lua/emilio/lazy/colors.lua
+++ lua/emilio/lazy/colors.lua
@@ -1,31 +1,8 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
+    'folke/tokyonight.nvim',
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme 'tokyonight-storm'
-        vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC', bold=true })
-        vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
-        vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#FB508F', bold=true })
-        require("tokyonight").setup({
-            style = "storm",
-            light_style = "day",
-            transparent = true,
-            terminal_colors = true,
-            styles = {
-                comments = { italic = true },
-                keywords = { italic = true },
-                functions = {},
-                variables = {},
-                sidebars = "dark",
-                floats = "dark",
-            },
-            sidebars = { "qf", "help" },
-            day_brightness = 0.3,
-            hide_inactive_statusline = false,
-            dim_inactive = false,
-            lualine_bold = false,
-        })
-    end,
+        vim.cmd.colorscheme 'tokyonight-night'
+    end
 }
 
