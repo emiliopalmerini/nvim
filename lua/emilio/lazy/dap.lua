33824a8e9f372f22d5d635160ee6e048f271881f
emiliopalmerini <emilio.palmerini@gmail.com>
2024-04-08 08:26:27 +0200

    fix(plugins): keymap command instead of functions I made a mistake in configuring the dap. This commit fixs it

--- lua/emilio/lazy/dap.lua
+++ lua/emilio/lazy/dap.lua
@@ -1,23 +1,55 @@
 return {
-    {
-        "mfussenegger/nvim-dap",
-        dependencies = {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "theHamsta/nvim-dap-virtual-text",
+        "nvim-telescope/telescope-dap.nvim",
+    },
+    config = function ()
+        --require('dap').setup({})
+
+        vim.keymap.set('n', '<F1>', ":lua require'dap'.continue()<CR>", {buffer=0})
+        vim.keymap.set('n', '<F2>', ":lua require'dap'.step_over()<CR>", {buffer=0})
+        vim.keymap.set('n', '<F3>', ":lua require'dap'.step_into()<CR>", {buffer=0})
+        vim.keymap.set('n', '<F3>', ":lua require'dap'.step_out()<CR>", {buffer=0})
+        vim.keymap.set('n', '<leader>b', ":lua require'dap'.toggle_breakpoint()<CR>", {buffer=0})
+        vim.keymap.set('n', '<leader>B', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", {buffer=0})
+        vim.keymap.set('n', '<leader>lp', ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>", {buffer=0})
+        vim.keymap.set('n', '<leader>dr', ":lua require'dap'.repl.open()<CR>", {buffer=0})
+
+        local dap = require('dap')
+        dap.adapters.delve = {
+            type = 'server',
+            port = '${port}',
+            executable = {
+                command = 'dlv',
+                args = {'dap', '-l', '127.0.0.1:${port}'},
+            }
+        }
+
+        -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+        dap.configurations.go = {
             {
-                "leoluz/nvim-dap-go"
+                type = "delve",
+                name = "Debug",
+                request = "launch",
+                program = "${file}"
             },
-            "rcarriga/nvim-dap-ui",
-            "theHamsta/nvim-dap-virtual-text",
-            "nvim-telescope/telescope-dap.nvim",
-        },
-        config = function ()
-            vim.keymap.set("n", "<F1>", ":lua require'dap'.continue()<CR>", {buffer=0})
-            vim.keymap.set("n", "<F2>", ":lua require'dap'.step_over()<CR>", {buffer=0})
-            vim.keymap.set("n", "<F3>", ":lua require'dap'.step_into()<CR>", {buffer=0})
-            vim.keymap.set("n", "<F3>", ":lua require'dap'.step_out()<CR>", {buffer=0})
-            vim.keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>", {buffer=0})
-            vim.keymap.set("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", {buffer=0})
-            vim.keymap.set("n", "<leader>lp", ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>", {buffer=0})
-            vim.keymap.set("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>", {buffer=0})
-        end
-    }
+            {
+                type = "delve",
+                name = "Debug test", -- configuration for debugging test files
+                request = "launch",
+                mode = "test",
+                program = "${file}"
+            },
+            -- works with go.mod packages and sub packages
+            {
+                type = "delve",
+                name = "Debug test (go.mod)",
+                request = "launch",
+                mode = "test",
+                program = "./${relativeFileDirname}"
+            }
+        }
+    end
 }
